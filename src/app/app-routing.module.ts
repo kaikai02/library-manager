7c18eab6868cd6ce312bbc1dcf6c8baf6428e1f2
Shiori <hontocha.prog@gmail.com>
2021-03-15 00:25:53 +0900

    feat(top-page): 一覧画面作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { TopPageComponent } from './top-page/top-page.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', component: TopPageComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'registration', component: RegistrationComponent },
 ];
